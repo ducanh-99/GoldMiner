93a3e7ae36c1be6e989ebb9b1e880ba8c2822337
ducanh <ducanhhust99@gmail.com>
2021-04-15 21:19:47 +0700

    delete assets file

--- Scripts/util/TagsManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class TagsManager : MonoBehaviour
-{
-    public static string GOLD_BIG = "GoldBig";
-    public static string GOLD_MEDIUM = "GoldMedium";
-}
