be9cfa1d100e5f1288446ab0bdad69e924847e91
xuanduonghoa <duonghx.vnist@gmail.com>
2021-05-07 22:44:53 +0700

    add particle system to levels

--- Scripts/component/BombSmokeScript.cs
+++ Scripts/component/BombSmokeScript.cs
@@ -10,23 +10,13 @@ public class BombSmokeScript : ObjectScripts
    
     void Start(){
         smoke.GetComponent<ParticleSystem>().enableEmission = false;
-        // smoke.GetComponent<ParticleSystem>().emission.enabled = false;
     }
     void OnTriggerEnter2D(Collider2D col)
     {
-        // base.OnTriggerEnter2D(col);
-        Debug.Log("OnTriggerEnter2D");
         if (col.CompareTag("Hook"))
         {
-            Debug.Log("Hihi");
             col.gameObject.GetComponent<Hook>().GetBombSmoke();
             smoke.GetComponent<ParticleSystem>().enableEmission = true;
-            StartCoroutine(stopSmoke());
-        }
-        if (col.tag == "Bomb")
-        {
-            Debug.Log(col.gameObject + " : " + gameObject.name);
-            Destroy(gameObject);smoke.GetComponent<ParticleSystem>().enableEmission = true;
             StartCoroutine(stopSmoke());
         }
     }
